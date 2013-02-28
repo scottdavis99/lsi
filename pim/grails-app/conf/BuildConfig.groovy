1e7537afd209618352fabb21f271241b00939630
John Flinchbaugh <jflinchbaugh@learningsciences.com>
2013-02-28 13:55:10 -0500

    added cobertura, spock

--- pim/grails-app/conf/BuildConfig.groovy
+++ pim/grails-app/conf/BuildConfig.groovy
@@ -41,6 +41,10 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
+        
+        test ":spock:0.7"
+        test ":code-coverage:1.2.5"
+                     
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
