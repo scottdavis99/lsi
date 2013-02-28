df0a1d7d443b979cc0189df9f1437ef4db0265a8
Brian Gaston <btgaston@yahoo.com>
2013-02-28 11:10:32 -0500

    added login form

---
+++ pim/grails-app/domain/net/lsi/User.groovy
@@ -0,0 +1,17 @@
+package net.lsi
+
+class User {
+
+
+    static constraints = {
+    }
+
+    String login
+    String password
+    String email
+    String name
+
+    String toString() {
+    	login
+    }
+}
